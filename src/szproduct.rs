de41ff7fb95185fa7debdd09ab2b31fa1dc031c3
docktermj <michael@dockter.com>
2025-12-02 16:13:55 -0500

    #1 Working async method

--- src/szproduct.rs
+++ src/szproduct.rs
@@ -1,39 +1,50 @@
 // use serde_json::Value;
-use szproduct_y::GetVersionRequest;
-use szproduct_y::sz_product_client::SzProductClient;
 
 pub mod szproduct_y {
+
     tonic::include_proto!("szproduct");
-}
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+    use sz_product_client::SzProductClient;
+    use tonic::transport::Channel;
 
-#[tokio::main]
-pub async fn get_version() -> Result<String, Box<dyn std::error::Error>> {
-    let mut client = SzProductClient::connect("http://0.0.0.0:8261").await?;
-    let request = tonic::Request::new(GetVersionRequest {});
-    let response = client.get_version(request).await?;
-    let response = response.get_ref().clone();
-    Ok(response.result)
+    pub struct SzProduct {
+        pub grpc_client: SzProductClient<Channel>,
+    }
+
+    impl SzProduct {
+        pub async fn get_version(&mut self) -> Result<String, Box<dyn std::error::Error>> {
+            let request = tonic::Request::new(GetVersionRequest {});
+            let response = self.grpc_client.get_version(request).await?;
+            Ok(response.get_ref().clone().result)
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::szproduct_y::sz_product_client::SzProductClient;
+    use tonic::transport::Channel;
 
-    // fn is_valid_json(s: String) -> bool {
-    //     println!("<<<<<<<< is_valid_json={:?}", s);
+    fn is_valid_json(s: String) -> bool {
+        println!(">>>>>> is_valid_json={:?}", s);
+        serde_json::from_str::<serde_json::Value>(&s).is_ok()
+    }
 
-    //     serde_json::from_str::<serde_json::Value>(&s).is_ok()
-    // }
+    async fn get_grpc_client() -> SzProductClient<Channel> {
+        match SzProductClient::connect("http://0.0.0.0:8261").await {
+            Ok(client) => client,
+            Err(_) => panic!("Cannot get gRPC channel"),
+        }
+    }
 
-    #[test]
-    fn test_get_version() {
-        let result = get_version();
-        println!(">>>>> is_valid_json: {:?}", result);
+    #[tokio::test]
+    async fn test_get_version() {
+        let client = get_grpc_client().await;
+        let mut szproduct = super::szproduct_y::SzProduct {
+            grpc_client: client,
+        };
 
-        // assert!(result.is_ok_and(is_valid_json));
+        let result = szproduct.get_version().await;
+        assert!(result.is_ok_and(is_valid_json));
     }
 }
